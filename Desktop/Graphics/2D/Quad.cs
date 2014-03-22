adacc421be081f1225ec994421bdc3261b607e8a
Jameson Ernst <j.patrick.ernst@gmail.com>
2014-03-21 18:30:38 -0700

    Rename flipY to flipV; add material property to quad.

--- Desktop/Graphics/2D/Quad.cs
+++ Desktop/Graphics/2D/Quad.cs
@@ -13,7 +13,7 @@ namespace GameStack.Graphics {
 		public Quad (Vector4 rect, Vector4 color, bool flipY = false) : this(null, rect, color, flipY) {
 		}
 
-		public Quad (Material material, Vector4 rect, Vector4 color, bool flipY = false) {
+		public Quad (Material material, Vector4 rect, Vector4 color, bool flipV = false) {
 			_material = material;
 			_color = color;
 			_vbuffer = new VertexBuffer (VertexFormat.PositionColorUV);
@@ -23,16 +23,18 @@ namespace GameStack.Graphics {
 				rect = new Vector4(-0.5f, -0.5f, 0.5f, 0.5f);
 
 			_vbuffer.Data = new [] {
-				rect.X, rect.Y, 0f, _color.X, _color.Y, _color.Z, _color.W, 0f, flipY ? 1f : 0f,
-				rect.Z, rect.Y, 0f, _color.X, _color.Y, _color.Z, _color.W, 1f, flipY ? 1f : 0f,
-				rect.Z, rect.W, 0f, _color.X, _color.Y, _color.Z, _color.W, 1f, flipY ? 0f : 1f,
-				rect.X, rect.W, 0f, _color.X, _color.Y, _color.Z, _color.W, 0f, flipY ? 0f : 1f
+				rect.X, rect.Y, 0f, _color.X, _color.Y, _color.Z, _color.W, 0f, flipV ? 1f : 0f,
+				rect.Z, rect.Y, 0f, _color.X, _color.Y, _color.Z, _color.W, 1f, flipV ? 1f : 0f,
+				rect.Z, rect.W, 0f, _color.X, _color.Y, _color.Z, _color.W, 1f, flipV ? 0f : 1f,
+				rect.X, rect.W, 0f, _color.X, _color.Y, _color.Z, _color.W, 0f, flipV ? 0f : 1f
 			};
 			_vbuffer.Commit ();
 			_ibuffer.Data = new [] { 0, 1, 2, 2, 3, 0 };
 			_ibuffer.Commit ();
 		}
 
+		public Material Material { get { return _material; } }
+		
 		protected override void OnDraw (ref Matrix4 world) {
 			var cam = ScopedObject.Find<Camera> ();
 			if (cam == null)
